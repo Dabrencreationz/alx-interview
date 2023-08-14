a31f4071286d6c17cec8014d6615e836576fe18e
dabrencreationz <brenvaltessy@yahoo.com>
2023-08-14 14:23:56 -0700

    rote 2d matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Rote 2D Matrix
+"""
+
+def rotate_2d_matrix(matrix):
+    n = len(matrix)
+
+    # Transpose the matrix
+    for a in range(n):
+        for b in range(a, n):
+            matrix[a][b], matrix[b][a] = matrix[b][a], matrix[a][b]
+
+    # Reverse each row
+    for a in range(n):
+        matrix[a] = matrix[a][::-1]
+
+# Example usage
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+rotate_2d_matrix(matrix)
+
+for row in matrix:
+    print(row)
---
+++ 0x07-rotate_2d_matrix/README.md
@@ -0,0 +1,4 @@
+# 0x07.Rotate 2D Matrix 
+
+# Task
+rotate n X n in a 2D matrix considering it's a square and moving in a clockwise motion.
