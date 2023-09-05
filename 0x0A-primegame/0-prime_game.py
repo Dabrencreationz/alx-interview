ed7c4abbf205bda3e94e58ec79939706369efa25
dabrencreationz <brenvaltessy@yahoo.com>
2023-09-05 15:38:59 +0000

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,46 +1,29 @@
 #!/usr/bin/python3
-"""Prime Game - Maria and Ben are playing a game"""
+""" Write a script for prime game"""
 
 
 def isWinner(x, nums):
-    """Determine the winner of the prime game.
-
-    Args:
-        x (int): Number of rounds.
-        nums (list): List of integers representing 'n' for each round.
-
-    Returns:
-        str: Name of the player who won the most rounds ('Ben', 'Maria'), or None if it's a tie.
-    """
-    if x <= 0 or nums is None or x != len(nums):
+    """Function that brings prime game to life"""
+    if not nums or x < 1:
         return None
-
-    ben_wins = 0
-    maria_wins = 0
-
-    is_prime = [1 for _ in range(max(nums) + 1)]
-    is_prime[0], is_prime[1] = 0, 0
-
-    for i in range(2, len(is_prime)):
-        if is_prime[i]:
-            rm_multiples(is_prime, i)
-
-    for i in nums:
-        if sum(is_prime[0:i + 1]) % 2 == 0:
-            ben_wins += 1
-        else:
-            maria_wins += 1
-
-    if ben_wins > maria_wins:
-        return "Ben"
-    elif maria_wins > ben_wins:
+    n = max(nums)
+   rm = [True for _ in range(max(n + 1, 2))]
+    for i in range(2, int(pow(n, 0.5)) + 1):
+        if not rm[i]:
+            continue
+        for j in range(i * i, n + 1, i):
+            rm[j] = False
+    rm[0] = rm[1] = False
+    c = 0
+    for i in range(len(rm)):
+        if rm[i]:
+            c += 1
+        rm[i] = c
+    plyr1 = 0
+    for n in nums:
+        plyr1 += rm[n] % 2 == 1
+    if plyr1 * 2 == len(nums):
+        return None
+    if plyr1 * 2 > len(nums):
         return "Maria"
-    else:
-        return None
-
-
-def rm_multiples(ls, x):
-    """Remove multiples of primes."""
-    for i in range(2 * x, len(ls), x):
-        ls[i] = 0
-print("The winner is:", winner)
+    return "Ben"
