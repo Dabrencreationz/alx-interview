5a9adf91fd638bab4fc40c89d2ca31dc7ad17df9
dabrencreationz <brenvaltessy@yahoo.com>
2023-09-05 12:35:31 +0000

    primegame

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,59 +1,46 @@
 #!/usr/bin/python3
+"""Prime Game - Maria and Ben are playing a game"""
 
 
 def isWinner(x, nums):
-    # Define a helper function to check if a number is prime
-    def is_prime(num):
-        if num <= 1:
-            return False
-        if num <= 3:
-            return True
-        if num % 2 == 0 or num % 3 == 0:
-            return False
-        i = 5
-        while i * i <= num:
-            if num % i == 0 or num % (i + 2) == 0:
-                return False
-            i += 6
-        return True
+    """Determine the winner of the prime game.
 
-    # Initialize the players' names and scores
-    maria_score = 0
-    ben_score = 0
+    Args:
+        x (int): Number of rounds.
+        nums (list): List of integers representing 'n' for each round.
 
-    # Loop through each round
-    for n in nums:
-        maria_turn = True  # Maria always goes first in each round
-        while n > 1:
-            prime_found = False
-            # Check for prime numbers and remove multiples
-            for i in range(2, n + 1):
-                if is_prime(i) and n % i == 0:
-                    n -= i
-                    prime_found = True
-                    break
-            if not prime_found:
-                break  # If no prime number is found, the player loses
+    Returns:
+        str: Name of the player who won the most rounds ('Ben', 'Maria'), or None if it's a tie.
+    """
+    if x <= 0 or nums is None or x != len(nums):
+        return None
 
-            # Switch turns
-            maria_turn = not maria_turn
+    ben_wins = 0
+    maria_wins = 0
 
-        # Update the scores based on the round winner
-        if maria_turn:
-            ben_score += 1
+    is_prime = [1 for _ in range(max(nums) + 1)]
+    is_prime[0], is_prime[1] = 0, 0
+
+    for i in range(2, len(is_prime)):
+        if is_prime[i]:
+            rm_multiples(is_prime, i)
+
+    for i in nums:
+        if sum(is_prime[0:i + 1]) % 2 == 0:
+            ben_wins += 1
         else:
-            maria_score += 1
+            maria_wins += 1
 
-    # Determine the overall winner
-    if maria_score > ben_score:
-        return "Maria"
-    elif maria_score < ben_score:
+    if ben_wins > maria_wins:
         return "Ben"
+    elif maria_wins > ben_wins:
+        return "Maria"
     else:
-        return None  # If there's a tie, return None
+        return None
 
-# Example usage:
-x = 3
-nums = [5, 7, 11]
-winner = isWinner(x, nums)
-print(f"The winner is:",{winner} winner)
+
+def rm_multiples(ls, x):
+    """Remove multiples of primes."""
+    for i in range(2 * x, len(ls), x):
+        ls[i] = 0
+print("The winner is:", winner)
